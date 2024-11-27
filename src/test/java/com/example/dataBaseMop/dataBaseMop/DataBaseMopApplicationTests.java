94ac6c08ad2e28bbe1303a505678e92fd99be272
NounceIWNL <nounceiwnl@gmail.com>
2024-11-27 16:13:31 +0300

    initial commit

---
+++ src/test/java/com/example/dataBaseMop/dataBaseMop/DataBaseMopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataBaseMop.dataBaseMop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseMopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6ca36ba25cc64600ba9e3d27c0afe93762801717
NounceIWNL <nounceiwnl@gmail.com>
2024-11-28 19:13:56 +0300

    GameClassService service has been created

---
+++ src/main/java/com/wowDataBase/WowDataBase/service/GameClassService.java
@@ -0,0 +1,23 @@
+package com.wowDataBase.WowDataBase.service;
+
+
+import com.wowDataBase.WowDataBase.modules.GameClass;
+import com.wowDataBase.WowDataBase.repository.GameClassRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameClassService {
+    @Autowired
+    private GameClassRepository gameClassRepository;
+
+    public List<GameClass> getAllGameClasses() {
+        return gameClassRepository.findAll();
+    }
+
+    public GameClass saveGameClass(GameClass gameClass) {
+        return gameClassRepository.save(gameClass);
+    }
+}

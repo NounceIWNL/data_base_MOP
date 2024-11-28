0c9874b07da42fba8245128fffc2ac604ee19cf6
NounceIWNL <nounceiwnl@gmail.com>
2024-11-28 19:11:25 +0300

    Tier model has been created

---
+++ src/main/java/com/wowDataBase/WowDataBase/modules/Tier.java
@@ -0,0 +1,42 @@
+package com.wowDataBase.WowDataBase.modules;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "tiers")
+public class Tier {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @JoinColumn(name="direction_id")
+    @ManyToOne(fetch=LAZY)
+    Direction direction;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+}

2e5516f0b995992896aec07a65770097708ddf78
NounceIWNL <nounceiwnl@gmail.com>
2024-11-28 19:09:44 +0300

    Direction model has been created

---
+++ src/main/java/com/wowDataBase/WowDataBase/modules/Direction.java
@@ -0,0 +1,28 @@
+package com.wowDataBase.WowDataBase.modules;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "directions")
+public class Direction {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

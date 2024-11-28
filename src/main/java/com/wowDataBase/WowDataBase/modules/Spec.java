3bbd0690edac8df0dd4005844b86874a6b9e890a
NounceIWNL <nounceiwnl@gmail.com>
2024-11-28 19:10:55 +0300

    Spec model has been created

---
+++ src/main/java/com/wowDataBase/WowDataBase/modules/Spec.java
@@ -0,0 +1,42 @@
+package com.wowDataBase.WowDataBase.modules;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "specs")
+public class Spec {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @JoinColumn(name="class_id")
+    @ManyToOne(fetch=LAZY)
+    GameClass gameClass;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public GameClass getGameClass() {
+        return gameClass;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGameClass(GameClass gameClass) {
+        this.gameClass = gameClass;
+    }
+}

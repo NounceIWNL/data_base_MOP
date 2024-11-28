2c2741062618733e08fa6814c1aa5d985f7ebe44
NounceIWNL <nounceiwnl@gmail.com>
2024-11-28 19:12:34 +0300

    GameClassController controller has been created

---
+++ src/main/java/com/wowDataBase/WowDataBase/controllers/GameClassController.java
@@ -0,0 +1,28 @@
+package com.wowDataBase.WowDataBase.controllers;
+
+import com.wowDataBase.WowDataBase.modules.GameClass;
+import com.wowDataBase.WowDataBase.service.GameClassService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/gameClasses")
+public class GameClassController {
+    @Autowired
+    private GameClassService gameClassService;
+
+    @GetMapping
+    public List<GameClass> getAllGameClasses() {
+        List<GameClass> gameClasses = gameClassService.getAllGameClasses();
+        System.out.println(gameClasses.toString());
+        return gameClasses;
+    }
+
+    @PostMapping
+    public GameClass createGameClass(@RequestBody GameClass gameClass) {
+        return gameClassService.saveGameClass(gameClass);
+    }
+}
